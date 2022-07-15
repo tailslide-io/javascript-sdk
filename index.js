6f4c492fdac1933fa4dd743e3868c9a8d478f1c7
Trent Cooper <00trent@protonmail.com>
2022-07-15 11:54:55 -0400

    Works on Toggler class, troubleshooting getting initial flag
    Co-authored-by: Elaine Vuong <e.vuong94@gmail.com>
    Co-authored-by: Jordan Eggers <jordan.shay.eggers@gmail.com>
    Co-authored-by: Steven Liou <stevenliou@gmail.com>

--- index.js
+++ index.js
@@ -9,6 +9,24 @@ const config = {
   const manager = new FlagManager(config);
   await manager.init();
 
+  // const flagConfig = {
+  //   flagName: 'Flag in app 9 number 2',
+  // };
+
+  // const FlagToggler = manager.newToggler(flagConfig);
+  // let counts = 0;
+  // let interval = setInterval(() => {
+  //   if (FlagToggler.isFlagActive()) {
+  //     console.log('Flag in App9 number 2 is active!');
+  //   } else {
+  //     console.log('Flag in App9 number 2 is not active!');
+  //   }
+  //   counts++;
+  //   if (counts > 5) {
+  //     clearInterval(interval);
+  //   }
+  // }, 5000);
+
   const cleanup = async () => {
     await manager.disconnect();
     console.log('closing nats');
