949e99246309c92d98391a1116ac76989180164c
Steven Liou <stevenliou@gmail.com>
2022-07-25 12:33:34 -0400

    Refactors FlagManger and NatsClient to listen to correct stream
    Co-authored-by: Elaine Vuong <e.vuong94@gmail.com>
    Co-authored-by: Trent Cooper <00trent@protonmail.com>
    Co-authored-by: Jordan Eggers <jordan.shay.eggers@gmail.com>

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const FlagManager = require('./lib/flagManager');
 
 const appId = 1;
-const flagName = 'Flag in app 1 number 1';
+const flagName = 'App 1 Flag 1';
 
 const config = {
   server: 'nats://127.0.0.1:4222',
   appId: 1,
   userContext: '375d39e6-9c3f-4f58-80bd-e5960b710295',
+  sdkKey: 'myToken',
 };
 
 // fbe6133c-f751-46f4-b275-4e8c3aa22e24 -> 84
@@ -24,13 +25,13 @@ const config = {
 
   const FlagToggler = manager.newToggler(flagConfig);
 
-  // if (FlagToggler.isFlagActive()) {
-  //   console.log(`Flag in ${appId} with name "${flagName}" is active!`);
-  //   await FlagToggler.emitSuccess();
-  // } else {
-  //   console.log(`Flag in ${appId} with name "${flagName}" is not active!`);
-  //   await FlagToggler.emitFailure();
-  // }
+  if (FlagToggler.isFlagActive()) {
+    console.log(`Flag in ${appId} with name "${flagName}" is active!`);
+    await FlagToggler.emitSuccess();
+  } else {
+    console.log(`Flag in ${appId} with name "${flagName}" is not active!`);
+    await FlagToggler.emitFailure();
+  }
 
   let count = 0;
   const interval = setInterval(async () => {
@@ -47,7 +48,7 @@ const config = {
     if (count > 20) {
       clearInterval(interval);
     }
-  }, 100);
+  }, 1000);
 
   const cleanup = async () => {
     await manager.disconnect();
